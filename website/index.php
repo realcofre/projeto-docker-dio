7b6afd2b297fc2c9555c1274dfc5ec9bb7600e9e
Renato Alexandre <renatoacf@gmail.com>
2023-07-03 23:47:40 -0300

    inicial

---
+++ website/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro projeto docker</title>
+</head>
+<body>
+    <?php echo "<h1>Hello World!</h1>"; ?>
+    <?php phpinfo(); ?>
+</body>
+</html>
